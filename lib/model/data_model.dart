705ad92978db97f750f3590c85bb7d9524e56c46
Nidhin <lalnidhinp02@gmail.com>
2023-02-14 11:51:27 +0530

    ui

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -2,23 +2,27 @@ import 'package:hive/hive.dart';
 part 'data_model.g.dart';
 
 @HiveType(typeId: 1)
-class StudentModel{
-
+class StudentModel {
   @HiveField(0)
-    String? id;
+  String? id;
 
   @HiveField(1)
   final String name;
 
-   @HiveField(2)
+  @HiveField(2)
   final String age;
 
-   @HiveField(3)
+  @HiveField(3)
   final String clas;
 
-   @HiveField(4)
+  @HiveField(4)
   final String address;
-  
-  StudentModel({required this.name, required this.age,this.id,required this.clas,required this.address});
 
+  StudentModel({
+    required this.name,
+    required this.age,
+    this.id,
+    required this.clas,
+    required this.address,
+  });
 }
